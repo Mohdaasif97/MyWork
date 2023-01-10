2c5d94eec5a8885412b94e1c76e08dd47b632a2c
Mohdaasif97 <122384084+Mohdaasif97@users.noreply.github.com>
2023-01-10 23:04:41 +0530

    Website using html and Css

---
+++ Project1/components/Info.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+export default function Info() {
+    return (
+        <>
+        < div className="info">
+            <div className="image">
+                <img src="https://th.bing.com/th/id/OIP.FgmWz271ZIyTKg_j9lzt7AHaF7?w=200&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" />
+                <div className="extra">
+                    <h1>UzumakI Naruto</h1>
+                    <p>front end developer</p>
+                                 <div className="button">
+                        <button>Email</button>
+                        <button className="link">Linkedin</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+
+}
